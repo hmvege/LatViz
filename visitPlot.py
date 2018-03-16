34b24d446061e1c6b761efafdaa43d9b14c23bf8
GioPede <giovanni.pederiva@gmail.com>
2018-03-16 13:49:17 -0400

    added color, gif and folders

--- visitPlot.py
+++ visitPlot.py
@@ -1,5 +1,7 @@
 import os
+import subprocess
 import numpy as np
+from colour import Color
 
 def initVisit(visitPyPath, visitBinPath):
     '''
@@ -33,11 +35,14 @@ def makeVideoAndGif(folder, outFileName):
     outFileName (string) -> the name of the output file
     '''
     # Create mp4 file
-    os.system("ffmpeg -r 1 -i " + folder + "/%02d.png -vcodec mpeg4 -r 10 -y " + outFileName)
+    cmd = ["ffmpeg", "-r", "8", "-i", folder + "temp/%03d.png", "-qscale:v", "0", "-r", "8", "-y", outFileName + ".avi"]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    read_out = proc.stdout.read()
     
     # Create gif file
-    # some command...
-
+    cmd = ["convert", "-delay", "10", "-loop", "0", folder + "temp/*.png", outFileName +".gif"]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    read_out = proc.stdout.read()
 
 def closeVisit():    
     '''
@@ -78,8 +83,8 @@ def makeFrame(fileName, observable, outputFile, minVal, maxVal):
     p.contourNLevels = 15
     
     # Define Color Scheme ? perhaps manually to get transparency?
-    p.colorType = 2
-    p.colorTableName = "hot"
+    colourPalette(p, p.contourNLevels)
+    p.colorType = 1
     visit.SetPlotOptions(p)
 
     # Title
@@ -128,20 +133,65 @@ def makeFrame(fileName, observable, outputFile, minVal, maxVal):
     visit.DeleteAllPlots()
     visit.CloseDatabase(fileName)
 
+def colourPalette(attributes, N):
+    red = Color("cyan")
+    colors = list(red.range_to(Color("red"),N))
+    for i, color in enumerate(colors):
+        colorTuple = [int(j * 255) for j in color.rgb]
+        colorTuple.append(50)
+        attributes.SetMultiColor(i, tuple(colorTuple))
+
+def splitFile(folder, inputConf, size):
+    cmd = ['mkdir', '-p', folder + "temp"]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    read_out = proc.stdout.read()
+    
+    cmd = ['split', "--bytes="+str(size**3 * 8), "-d", "-a", "3", "--additional-suffix=.splitbin", folder+inputConf, folder+"temp/file"]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    read_out = proc.stdout.read()
+
+    files = []
+    for i in xrange(2*size):
+        with open(folder+"temp/"+str(i).zfill(3)+".bov", 'w') as f:
+            f.write("TIME: "+str(i)+"\n\
+DATA_FILE: file" + str(i).zfill(3)+ ".splitbin \n\
+DATA_SIZE: " + str(size) + " " + str(size) + " " + str(size) + "\n\
+DATA_FORMAT: DOUBLE \n\
+VARIABLE: field \n\
+DATA_ENDIAN: LITTLE \n\
+CENTERING: zonal \n\
+BRICK_ORIGIN: 0. 0. 0. \n\
+BRICK_SIZE: " + str(size) + ". " + str(size) + ". " + str(size) + ".")
+        file = folder+"temp/" + str(i).zfill(3) + ".bov"
+        files.append(file)
+    return files 
+
+def plotConf(folder, inputConf, size, observable, outpuFileName):
+    
+    # Split file into sub-blocks
+    files = splitFile(folder, inputConf, size)
+
+    # Plot Frames
+    for i, file in enumerate(files):
+     makeFrame(file,"energy", folder + "temp/" + str(i).zfill(3), 0.01, 0.2)
+
+    # Make Video
+    makeVideoAndGif(folder, folder + outpuFileName)
+    
+    # Clean up
+    os.system("rm -r " + folder + "temp")
+
+
 if __name__ == "__main__":
-    # Initialize Visit
+    
+    # # Initialize Visit
     visitBinPath = "/home/giovanni/Desktop/visit2_13_0.linux-x86_64/bin"
     visitPyPath = "/home/giovanni/Desktop/visit2_13_0.linux-x86_64/2.13.0/linux-x86_64/lib/site-packages"
     initVisit(visitPyPath, visitBinPath)
 
-    # Plot Frames
-    makeFrame("00.bov","energy", "00", 0.01, 0.1)
-    makeFrame("01.bov","energy","01", 0.01, 0.1)
-    makeFrame("02.bov","energy","02", 0.01, 0.1)
-    makeFrame("03.bov","energy","03", 0.01, 0.1)
-
-    # Close Visit
+    # Plot
+    plotConf(os.path.abspath("a/b/")+"/", "field.bin", 32, "energy", "field")
+    
+    # Close Visit and Delete temp files
     closeVisit()
 
-    # Make Video
-    makeVideoAndGif(".", "a.mp4")
